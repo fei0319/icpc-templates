b13567eee9c056e32505d42a864ad34342ce4358
Fei Pan <pan-fei@outlook.com>
2024-10-16 12:36:52 +0800

    added sam

---
+++ src/sam.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+template <int SIGMA>
+struct SuffixAutomaton {
+    struct Node {
+        int len{};
+        int link{};
+        int next[SIGMA]{};
+    };
+    std::vector<Node> t;
+    int tot;
+    explicit SuffixAutomaton(int size) : t(size), tot(1) {
+        std::fill(t[0].next, t[0].next + SIGMA, 1);
+        t[0].len = -1;
+    }
+    int extend(int p, int c) {
+        if (t[p].next[c]) {
+            int q = t[p].next[c];
+            if (t[q].len == t[p].len + 1)
+                return q;
+            int r = ++tot;
+            t[r].len = t[p].len + 1;
+            t[r].link = t[q].link;
+            std::copy(t[q].next, t[q].next + SIGMA, t[r].next);
+            t[q].link = r;
+            while (t[p].next[c] == q) {
+                t[p].next[c] = r;
+                p = t[p].link;
+            }
+            return r;
+        }
+        int cur = ++tot;
+        t[cur].len = t[p].len + 1;
+        while (!t[p].next[c]) {
+            t[p].next[c] = cur;
+            p = t[p].link;
+        }
+        t[cur].link = extend(p, c);
+        return cur;
+    }
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string s;
+    std::cin >> s;
+
+    auto sam = SuffixAutomaton<26>{std::max<int>(s.size() * 2, 10)};
+
+    int last = 0;
+    for (auto c : s) {
+        last = sam.extend(last, c - 'a');
+    }
+}
