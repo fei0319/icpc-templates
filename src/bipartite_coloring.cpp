2f372c31f02311f14935863d57a2d7e65247bf40
Fei Pan <pan-fei@outlook.com>
2024-10-30 00:14:05 +0800

    bipartite_coloring: shrinked

--- src/bipartite_coloring.cpp
+++ src/bipartite_coloring.cpp
@@ -10,25 +10,15 @@ struct BipartiteColoring {
     int operator()() {
         const int n = adj.size();
         std::vector<int> deg(n);
-
-        for (auto [u, v] : edge) {
-            ++deg[u];
-            ++deg[v];
-        }
-
+        for (auto [u, v] : edge) ++deg[u], ++deg[v];
         const int d = *std::max_element(deg.begin(), deg.end());
         adj.assign(n, std::vector<int>(d, -1));
-
         for (int i = 0; i < edge.size(); ++i) {
             int id = i;
             auto [x, y] = edge[id];
             int cx = 0, cy = 0;
-            while (adj[x][cx] != -1) {
-                ++cx;
-            }
-            while (adj[y][cy] != -1) {
-                ++cy;
-            }
+            while (adj[x][cx] != -1) ++cx;
+            while (adj[y][cy] != -1) ++cy;
             while (id != -1) {
                 int n_id = adj[y][cx];
                 if (n_id == -1) {
@@ -36,9 +26,7 @@ struct BipartiteColoring {
                     break;
                 }
                 auto [n_x, n_y] = edge[n_id];
-                if (n_x != y) {
-                    std::swap(n_x, n_y);
-                }
+                if (n_x != y) std::swap(n_x, n_y);
                 adj[n_x][cx] = adj[n_y][cx] = -1;
                 adj[x][cx] = adj[y][cx] = id;
 
@@ -47,15 +35,10 @@ struct BipartiteColoring {
                 std::swap(cx, cy);
             }
         }
-
         color.resize(edge.size());
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < d; ++j) {
-                if (adj[i][j] != -1) {
-                    color[adj[i][j]] = j;
-                }
-            }
-        }
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < d; ++j)
+                if (adj[i][j] != -1) color[adj[i][j]] = j;
         return d;
     }
 };
