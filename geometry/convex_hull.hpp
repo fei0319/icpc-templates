c48d70c41d6f26e4a301c138fa853d21c9398df0
Fei Pan <pan-fei@outlook.com>
2024-10-31 21:22:54 +0800

    convex_hull: shrinked

--- geometry/convex_hull.hpp
+++ geometry/convex_hull.hpp
@@ -7,20 +7,14 @@ auto make_hull(std::vector<P> p) {
     std::vector<P> hi, lo;
     for (auto p : p) {
         while (hi.size() > 1 &&
-               (hi.back() - hi[hi.size() - 2]) * (p - hi.back()) >= 0) {
+               (hi.back() - hi[hi.size() - 2]) * (p - hi.back()) >= 0)
             hi.pop_back();
-        }
-        while (!hi.empty() && hi.back().x == p.x) {
-            hi.pop_back();
-        }
+        while (!hi.empty() && hi.back().x == p.x) hi.pop_back();
         hi.push_back(p);
         while (lo.size() > 1 &&
-               (lo.back() - lo[lo.size() - 2]) * (p - lo.back()) <= 0) {
+               (lo.back() - lo[lo.size() - 2]) * (p - lo.back()) <= 0)
             lo.pop_back();
-        }
-        if (lo.empty() || lo.back().x < p.x) {
-            lo.push_back(p);
-        }
+        if (lo.empty() || lo.back().x < p.x) lo.push_back(p);
     }
     return std::make_pair(hi, lo);
 }
